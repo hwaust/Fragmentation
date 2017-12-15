1f532185067d38c8a4d8093c8b1bc61445102446
hao <hao@Haos-MacBook-Pro.local>
2017-12-15 21:16:11 +0800

    XQuerytest

---
+++ src/fragmentation/tests/XQueryTest.java
@@ -0,0 +1,18 @@
+package fragmentation.tests;
+
+import basex.BXClient;
+
+public class XQueryTest {
+
+	public static void main(String[] args) throws Exception {
+
+		for (int i = 0; i < 256; i++) {
+			System.out.printf("%3d,%2x,%c\n", i, i, i);
+		}
+	}
+
+	public static void test1() throws Exception {
+
+	}
+
+}
